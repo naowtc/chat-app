65e07e4669b880386d44cf17a1e7fbbb37f37b27
naowtc <naowtc@gmail.com>
2020-09-10 09:24:20 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,8 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    #チャットルームに紐付いている全てのメッセージ（@room.messages）を@messagesと定義し全てのメッセージ情報に紐づくユーザー情報を、includes(:user)と記述
+    @messages = @room.messages.includes(:user)
   end
 #DBに値を保存できるように、createアクションを実装
   #@room.messages.newでチャットルームに紐づいたメッセージのインスタンスを生成し、message_paramsを引数にして、privateメソッドを呼び出す
@@ -12,6 +14,7 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path(@room)
     else
+      @messages = @room.messages.includes(:user)
       render :index
     end
   end
